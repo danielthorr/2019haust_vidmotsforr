6e30e1575334028b63f1a87415eccb3858ada3f1
Daniel Thor Thorisson <danielthorr@gmail.com>
2019-09-03 15:11:03 +0000

    Added animations
    Added animation to the arrow and bow falling into place on mouseenter and mouseleave doing the reverse. Also started adding animations for bow being drawn back

--- Verkefni1_Logo/2_animated/jslogo.js
+++ Verkefni1_Logo/2_animated/jslogo.js
@@ -1,7 +1,9 @@
+"use strict";
 function elById(id) {
   return document.getElementById(id);
 }
 
+let svg = document.querySelector("svg");
 let arrowFeathers = elById("arrowFeather");
 let arrow = elById("arrow");
 let bow = elById("bow");
@@ -9,9 +11,52 @@ let bowTop = elById("bowTop");
 let bowBottom = elById("bowBottom")
 let bowString = elById("bowString");
 
-
-anime({
-
+svg.addEventListener("mouseenter", function() {
+  anime.remove(arrow);
+  anime({
+  targets: arrow,
+  transform: "rotate(20) translate(50, -40) scale(0.8)",
+  duration: 200,
+  easing: "easeOutQuad",
+  });
+  anime({
+    targets:bow,
+    transform: "translate(95, 0)",
+    easing: "linear",
+    duration: 150
+  });
 });
-<script src="node_modules/animejs/lib/anime.min.js"></script>
-<script src="jslogo.js"></script>
+
+svg.addEventListener("mouseleave", function() {
+  anime.remove(arrow);
+  anime({
+  targets: arrow,
+  transform: "rotate(45) translate(70, -160) scale(0.8)",
+  duration: 200,
+  easing: "easeOutQuad"
+  });
+  anime({
+    targets:bow,
+    transform: "translate(70, 0)",
+    easing: "linear",
+    duration: 150
+  });
+});
+
+svg.addEventListener("mousedown", function() {
+  anime({
+    targets: arrow,
+    transform: "rotate(0) translate(-73, 10) scale(0.8)",
+    duration: 300,
+    easing: "easeOutQuad",
+    complete: function() {
+      anime({
+        targets: bow,
+        transform: "translate(0, 0)",
+        easing: "cubicBezier(0.075, 0.025, 0.195, 0.985)",
+        duration: 1500
+      })
+    }
+    }
+  );
+});
